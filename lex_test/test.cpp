50acf96b9715b5d88d858430ab9aebd895a1cac2
NikeNoMore <128460679+NikeNoMore@users.noreply.github.com>
2024-02-29 21:56:00 +0300

    Update test.cpp

--- lex_test/test.cpp
+++ lex_test/test.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, TestName1) {
 	filebuf fb;
 	if (fb.open("test1.txt", ios::in)) {
 		istream is(&fb);
@@ -16,7 +16,32 @@ TEST(TestCaseName, TestName) {
 			Lexem l = lex.getNextLexem();
 			res.push_back(l);
 		}
-		EXPECT_EQ(res, vector<Lexem> { {"kw", "int"}, { "id", "k" }, { "opeq", "" }, { "num", "3" }, { "semicolon", "" }});
+		vector<Lexem> check{ {"kw", "int"}, { "id", "k" }, { "opeq", "" }, { "num", "3" }, { "semicolon", "" } };
+		ASSERT_EQ(res.size(), check.size());
+		for (int i = 0; i < res.size(); i++) {
+			EXPECT_EQ(res[i], check[i]);
+		}
+		fb.close();
+	}
+}
+
+TEST(TestCaseName, TestName2) {
+	filebuf fb;
+	if (fb.open("test2.txt", ios::in)) {
+		istream is(&fb);
+		Lexer lex(is);
+		vector<Lexem> res;
+		while (is) {
+			Lexem l = lex.getNextLexem();
+			res.push_back(l);
+		}
+		vector<Lexem> check{ {"kw", "int"}, { "id", "a" }, { "opeq", "" }, { "num", "1" }, { "semicolon", "" },
+		{"kw", "int"}, { "id", "b" }, { "opeq", "" }, { "num", "2" }, { "semicolon", "" },
+		{"kw", "int"}, { "id", "c" }, { "opeq", "" }, { "id", "a" }, {"opplus", ""}, {"id", "b"}, {"semicolon", ""}};
+		ASSERT_EQ(res.size(), check.size());
+		for (int i = 0; i < res.size(); i++) {
+			EXPECT_EQ(res[i], check[i]);
+		}
 		fb.close();
 	}
 }
