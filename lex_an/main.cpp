ac9d66dbb4dd946ee16d3cb988081aa08c423e70
NikeNoMore <128460679+NikeNoMore@users.noreply.github.com>
2024-03-01 13:17:00 +0300

    Update main.cpp

--- lex_an/main.cpp
+++ lex_an/main.cpp
@@ -15,36 +15,46 @@ int main()
 
 ===============================================
 
+#include "../stat_lib/lexer_frm.h"
 #include <iostream>
 #include <queue>
 #include <string>
 #include <vector>
 
 using namespace std;
-using Lexem = pair<string, string>;
 
 int main()
 {
+    Lexer lex(cin);
+    vector<Lexem> data;
+    Lexem l = lex.getNextLexem();
+    data.push_back(l);
+    while (l != Lexem {"", ""} ) {
+        l = lex.getNextLexem();
+        data.push_back(l);
+    }
     vector<string> res;
     vector<string> stack;
-    vector<Lexem> data = { {"num", "2"}, {"opplus", ""}, {"num", "3"}, {"opmul", ""},  {"num", "4"} };
+    int arg = 0;
     for (auto& x : data) {
         if (x.first == "num") {
             res.push_back(x.second);
+            arg += 1;
         }
         else if (x.first == "opplus") {
             if (stack.empty()) {
                 stack.push_back(x.first);
             }
             else {
-                for (int y = 0; y < stack.size(); y++) {
-                    res.push_back(stack.back());
+                for (int y = stack.size() - 1; y >= 0; y--) {
+                    res.push_back(stack[y]);
                     stack.pop_back();
+                    arg -= 1;
                 }
                 stack.push_back(x.first);
             }
         }
-        else {
+        else if (x.first == "opmul") {
             if (stack.empty()) {
                 stack.push_back(x.first);
             }
@@ -53,10 +63,32 @@ int main()
             }
             else {
                 res.push_back("opmul");
+                arg -= 1;
+
             }
         }
     }
-    for (auto& x : res) {
-        cout << x;
+    if (!stack.empty()) {
+        for (int y = stack.size() - 1; y >= 0; y--) {
+            res.push_back(stack[y]);
+            arg -= 1;
+            stack.pop_back();
+        }
+    }
+    if (arg != 1) {
+        cout << "ERROR";
+    }
+    else {
+        for (auto& x : res) {
+            if (x == "opplus") {
+                cout << "+ ";
+            }
+            else if (x == "opmul") {
+                cout << "* ";
+            }
+            else {
+                cout << x << " ";
+            }
+        }
     }
 }
