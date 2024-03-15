6e14207a724e0c02e21423bf30a21f6a5801b3a0
NikeNoMore <128460679+NikeNoMore@users.noreply.github.com>
2024-03-15 13:35:24 +0300

    Add files via upload
    Pizdec

---
+++ syntax_an/syntax_an.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+using Lexem = pair<string, string>;
+
+int main()
+{
+    /*
+    vector<Lexem> correct = {
+            { "kw", "int" },
+            { "id", "main" },
+            { "lpar", "" },
+            { "rpar", "" },
+            { "lbrace", "" },
+            { "kw", "return" },
+            { "num", "0" },
+            { "semicolon", "" },
+            { "rbrace", "" },
+            { "end", "" }
+    };*/
+    vector<Lexem> expr = {
+        {"num", "1"},
+        {"opplus", ""},
+        {"num", "2"}
+    };
+    for (int x = int(expr.size()) - 1; x >= 0; x--) {
+        cout << expr[x].first;
+    }
+}
